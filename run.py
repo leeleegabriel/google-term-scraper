bc15d37995c04bbadfc6de7427842e73cdf65df2
leeleegabriel <lgv0001@auburn.edu>
2018-12-03 22:05:09 -0600

    cleaned up run

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ import time
 import logging
 # import configparser
 import asyncio
-import datetime
+#import datetime
 from time import sleep
-from concurrent.futures import ProcessPoolExecutor
+#from concurrent.futures import ProcessPoolExecutor
 
 import lib.Helper as Helper
 from lib.Scraper import Scraper
@@ -43,30 +43,39 @@ logger.addHandler(ch)
 
 DB_timeout = 60
 version = 'v0.5'
+
 Download_dir = './downloads/'
 Config_dir = './config/'
-DB = 'ScrapeDB.db'
-Word_list = 'words.txt'
-Filetypes = 'filetypes.txt'
-ChromeDriver = 'chromedriver'
+
+DB = Download_dir + 'ScrapeDB.db'
+Words = Config_dir + 'words.txt'
+Filetypes = Config_dir + 'filetypes.txt'
+ChromeDriver = Config_dir + 'chromedriver'
+
+App_dir = Download_dir + 'unfiltered/app/'
+Misc_dir = Download_dir + 'unfiltered/misc/'
+
+Sample_dir = Download_dir + 'samples/'
+Hit_dir = Download_dir + 'filtered/hit/'
+Miss_dir = Download_dir + 'filtered/miss/'
+Error_dir = Download_dir + 'filtered/error'
 
 
 def main():
 	logger.info('Starting GTS %s at %s' % (version, time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime())))
-	Sample_dir, Unfiltered_dir, App_dir, Misc_dir, Hit_dir, Miss_dir, Error_dir, CSearch_dir, DB_file, Words_file, Filetypes_file, ChromeDriver_file = setupDirs()
-	Filter_errors, Words_file = getArgs(Words_file)
-	setupDB(DB_file)
+	Filter_errors, Words_file = getArgs(Words)
+	setupDB(DB)
 
 	loop = asyncio.get_event_loop()
 	#executor = ProcessPoolExecutor()
 
-	scraper = Scraper(logger, DB_file, ChromeDriver_file, Filetypes_file, Words_file)
+	scraper = Scraper(logger, DB, ChromeDriver, Filetypes, Words)
 	future_S = loop.run_in_executor(None, scraper.run, loop)
 
-	downloader =Downloader(logger, scraper, DB_file, Filter_errors, App_dir, Misc_dir)
+	downloader = Downloader(logger, scraper, DB, Filter_errors, App_dir, Misc_dir)
 	future_d = loop.run_in_executor(None, downloader.run, loop)
 
-	filterer = Filterer(logger, scraper, Sample_dir, Hit_dir, Miss_dir, Error_dir, Unfiltered_dir, Words_file)
+	filterer = Filterer(logger, scraper, Sample_dir, Hit_dir, Miss_dir, Error_dir, Unfiltered_dir, Words)
 	future_f = loop.run_in_executor(None, filterer.run, loop)
 
 	try:
@@ -79,42 +88,20 @@ def main():
 	while not scraper.fin or not downloader.fin or not filterer.fin:
 		pass
 	loop.close()
-	logger.info('Fin.')	
+	logger.info('Fin.')
 
 
-def setupDirs():
-	DBase_dir = Download_dir
-	CBase_dir = Config_dir
-
-	Sample_dir = DBase_dir + 'samples/'
-	Unfiltered_dir = DBase_dir + '/Unfiltered_dir/'
-	App_dir = DBase_dir + 'unfiltered/app/'
-	Misc_dir = DBase_dir + 'unfiltered/misc/'
-	Hit_dir = DBase_dir + 'filtered/hit/'
-	Miss_dir = DBase_dir + 'filtered/miss/'
-	Error_dir = DBase_dir + 'filtered/error/'
-	CSearch_dir = CBase_dir + 'search/'
-
-	dirs = [DBase_dir, CBase_dir, Sample_dir, Unfiltered_dir, App_dir, Misc_dir, Hit_dir, Miss_dir, Error_dir, CSearch_dir]
-	for dire in dirs:
-		Helper.makeFolder(dire)
-
-	DB_file = CBase_dir + DB
-	Words_file = CBase_dir + Word_list
-	Filetypes_file = CSearch_dir + Filetypes
-	ChromeDriver_file = CBase_dir + ChromeDriver
-
-	files = [Words_file, Filetypes_file, ChromeDriver_file]
+def verifyFiles():
+	files = [Words, Filetypes, ChromeDriver]
 	missing_files = [f for f in files if not Helper.checkFile(f)]
 	if missing_files:
 		logger.error('missing files: %s' % missing_files)
 		sys.exit(0)
-	return Sample_dir, Unfiltered_dir, App_dir, Misc_dir, Hit_dir, Miss_dir, Error_dir, CSearch_dir, DB_file, Words_file, Filetypes_file, ChromeDriver_file
 
 
 def getArgs(Words_file):
 	parser = argparse.ArgumentParser(description='google-term-scraper', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('-de', '--filter_errors', default=False, action='store_true', help='Don\'t attempt to download previously attempted URLs with errors')
+	parser.add_argument('-fe', '--filter_errors', default=False, action='store_true', help='Don\'t attempt to download previously attempted URLs with errors')
 	parser.add_argument('-w', '--word_file', default=Words_file, help='word list name in config/search/ for generating queries')
 	args = parser.parse_args()
 
@@ -159,5 +146,5 @@ if __name__ == '__main__':
 	except ImportError as e:
 		logger.error('\nError importing, %s\n' % e.message)
 		sys.exit(1)
+	verifyFiles()
 	main()
-
